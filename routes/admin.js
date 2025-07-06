b80b56c98e7c78d5675942131f868c9689f82550
akshitajaiswal2908 <akshitajaiswal2908@gmail.com>
2025-07-06 13:48:47 +0530

    section 8 complte

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const router = express.Router();
 const path = require('path');
-const productsController = require('../controllers/product');
 
-const products = [];
+const express = require('express');
 
+const adminController = require('../controllers/admin');
 
-router.get('/add-product',productsController.getAddProduct);
+const router = express.Router();
 
-router.post('/add-product',productsController.postAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-// exports.routes = router;
-// exports.products = products;
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
